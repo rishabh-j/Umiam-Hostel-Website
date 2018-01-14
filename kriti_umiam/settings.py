602e0ab39190cabdd2c35c84d725f9cc14e12afd
shubham-goel99 <32568939+shubham-goel99@users.noreply.github.com>
2018-01-14 21:00:34 +0530

    Update settings.py

--- kriti_umiam/settings.py
+++ kriti_umiam/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mjy5qg-l4jav4_iojqc*n&1sl^r1y0@v(5_3o2*w7ktw#13py!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
